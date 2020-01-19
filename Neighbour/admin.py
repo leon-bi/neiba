b4e00251f7e338f150328413c7d62e665e06605c
bchizi <leonbichanga@gmail.com>
2020-01-19 19:14:35 +0300

    added posts to the admin so to be able to edit the in the admin page for django

--- Neighbour/admin.py
+++ Neighbour/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from Neighbour.models import UserProfileInfo
-
+from Neighbour.models import UserProfileInfo,Post
 # Register your models here.
-admin.site.register(UserProfileInfo)
+admin.site.register(UserProfileInfo) 
+admin.site.register(Post) 
 
