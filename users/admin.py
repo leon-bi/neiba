4af31bdf4d915f03a87e47c537950be462a27a00
bchizi <leonbichanga@gmail.com>
2020-01-21 17:02:36 +0300

    added users to the admin.site for admin

--- users/admin.py
+++ users/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from .models import Profile
+from django.contrib.auth.admin import UserAdmin
 # Register your models here.
-admin.site.register(Profile)
+admin.site.register(Profile) 
+
+
+
